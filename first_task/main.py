290ef0305610b053bdba25f56ebd76385af5bf18
Nikita Zontov <zontovnikita@gmail.com>
2024-09-07 14:33:30 +0300

    argparse and data_filter

---
+++ first_task/main.py
@@ -0,0 +1,11 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Скрипт для предсказания и сохранения результатов модели.')
+
+parser.add_argument('test_file', type=str, help='Путь к файлу test.csv')
+
+parser.add_argument('submission_file', type=str, help='Путь для сохранения файла submission.csv')
+
+args = parser.parse_args()
+
+
